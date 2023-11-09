00faf10eeeec14b6ca228cc95ce1e158814e74be
Tanvir Hasan <57188940+Tanvir-art@users.noreply.github.com>
2023-11-09 15:03:36 +0600

    create deleted api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const  express = require('express');
 const cors = require('cors');
@@ -68,6 +68,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/wishlist/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result =  await wishlistCollection.deleteOne(query);
+      res.send(result);
+    })
+ 
 
 
     // Send a ping to confirm a successful connection
